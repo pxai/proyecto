7dd9f3770d3d4ebe8fc61db2d9cc35207edf2cc1
Pello Altadill <pello.altadill@gluckgames.com>
2019-07-08 01:17:10 +0200

    Changed hello.js

--- hello.js
+++ hello.js
@@ -1,9 +1,8 @@
-/**
- hello.js
+/*
+//  hello.js
 */
-
-function hello () {
-  console.log("Hello there");
+function sayHello (name="default"){
+  console.log("Hello there: " + name);
 }
 
-hello();
+hello("Juan");
